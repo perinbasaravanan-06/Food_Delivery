a809ec971d9d475b43e66f6124fbf5a3918aaa41
perinbasaravanan-06 <perinbasaravanank06@gmail.com>
2025-10-23 23:17:08 +0530

    image resolved 10

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,9 +20,6 @@ const App = () => {
       <div className="app-content">
         <SideBar/>
         <Routes>
-          <Route path="/" element={<Home />} />
-
-
           <Route path="/add" element={<Add   url = {url}/>}/>
           <Route path="/list" element={<List url = {url}/>}/>
           <Route path="/orders" element={<Orders url = {url}/>}/>
