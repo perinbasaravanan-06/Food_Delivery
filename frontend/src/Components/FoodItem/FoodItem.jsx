c690ecddc23f8e0feb5fc6e559c5cfeee018e6d1
perinbasaravanan-06 <perinbasaravanank06@gmail.com>
2025-10-24 00:04:49 +0530

    image resolved 14

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -25,7 +25,7 @@ const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
                 <img src={assets.rating_starts} alt="" />
             </div>
             <p className="food-item-desc">{description}</p>
-            <p className='food-item-price'>${price}</p>
+            <p className='food-item-price'>${price || 20}</p>
         </div>
     </div>
   )
